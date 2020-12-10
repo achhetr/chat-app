4d23773e23d7b09a5f78d3c75e970ddfd3eff697
Akash Chhetri <akash.chhetri1@gmail.com>
2020-12-11 10:32:08 +1100

    user function added

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,6 +1,42 @@
 const users = [];
 
-const addUser = () => {};
-const getUser = () => {};
-const removeUser = () => {};
-const getUsersInRoom = () => {};
+const addUser = ({ id, username, room }) => {
+	username = username.trim().toLowerCase();
+	room = room.trim().toLowerCase();
+
+	if (!username || !room) {
+		return {
+			error: 'Username and room are required',
+		};
+	}
+
+	const existingUser = users.find((user) => {
+		return user.room === room && user.username === username;
+	});
+
+	if (existingUser) {
+		return {
+			error: 'Username already exists',
+		};
+	}
+
+	const user = { id, username, room };
+	users.push(user);
+
+	return { user };
+};
+
+const removeUser = (id) => {
+	const index = users.findIndex((user) => user.id === id);
+
+	if (index === -1) {
+		return users.splice(index, 1)[0];
+	}
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => {
+	const userRoom = users.filter((user) => user.room === room);
+	return userRoom;
+};
