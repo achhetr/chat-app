56e3f74f7a4ca8e9c7d33e25803da73bc8ba95c9
Akash Chhetri <akash.chhetri1@gmail.com>
2020-12-10 15:03:33 +1100

    acknowledgement added

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,7 +8,12 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
 	e.preventDefault();
 	const message = e.target.elements.message.value;
 	e.target.elements.message.value = '';
-	socket.emit('sendMessage', message);
+	socket.emit('sendMessage', message, (error) => {
+		if (error) {
+			return console.log(error);
+		}
+		console.log('Message delivered!');
+	});
 });
 
 document.querySelector('#send-location').addEventListener('click', () => {
@@ -17,9 +22,15 @@ document.querySelector('#send-location').addEventListener('click', () => {
 	}
 	navigator.geolocation.getCurrentPosition((position) => {
 		const { latitude, longitude } = position.coords;
-		socket.emit('sendLocation', {
-			latitude,
-			longitude,
-		});
+		socket.emit(
+			'sendLocation',
+			{
+				latitude,
+				longitude,
+			},
+			() => {
+				console.log('Location shared');
+			}
+		);
 	});
 });
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http');
 require('dotenv').config();
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -19,15 +20,22 @@ io.on('connection', (socket) => {
 	socket.emit('message', 'Welcome!');
 	socket.broadcast.emit('message', 'A new user has joined!');
 
-	socket.on('sendMessage', (message) => {
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter();
+
+		if (filter.isProfane(message)) {
+			return callback('Profanity is not allowed!');
+		}
 		io.emit('message', message);
+		callback();
 	});
 
-	socket.on('sendLocation', ({ latitude, longitude }) => {
+	socket.on('sendLocation', ({ latitude, longitude }, callback) => {
 		socket.broadcast.emit(
 			'message',
 			`User location: https://google.com/maps?q=${latitude},${longitude}`
 		);
+		callback();
 	});
 
 	socket.on('disconnect', () => {
