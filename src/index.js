80f8b37212dcfc27800c6ff1d47192947f52a6c7
Akash Chhetri <akash.chhetri1@gmail.com>
2020-12-10 13:47:23 +1100

    socket connected to front end

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const http = require('http');
 require('dotenv').config();
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT;
 const publicDirectory = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectory));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+	console.log('New WebSocket connection');
+});
+
+server.listen(port, () => {
 	console.log(`Listening to port ${port}!`);
 });
