fd1ed1b1845697928980d8531fc301cbe9160577
Akash Chhetri <akash.chhetri1@gmail.com>
2020-12-10 14:33:06 +1100

    user connection notification added

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,7 +6,7 @@ socket.on('message', (message) => {
 
 document.querySelector('#message-form').addEventListener('submit', (e) => {
 	e.preventDefault();
-	const message = document.querySelector('input').value;
-	document.querySelector('input').value = '';
+	const message = e.target.elements.message.value;
+	e.target.elements.message.value = '';
 	socket.emit('sendMessage', message);
 });
